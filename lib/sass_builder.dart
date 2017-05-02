baec0bb22f5ea19afc466252ed8775eb7a7d6fed
Luis Vargas <luisvargastijerino@gmail.com>
2017-05-01 21:19:07 -0400

    upgrade build to `^0.9.1` and build_runner to `^0.3.2`

--- lib/sass_builder.dart
+++ lib/sass_builder.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
-import 'dart:io';
-
+import 'dart:convert';
 import 'package:build/build.dart';
 import 'package:logging/logging.dart';
 import 'package:package_resolver/package_resolver.dart';
@@ -8,32 +7,67 @@ import 'package:path/path.dart';
 import 'package:sass/sass.dart';
 
 class SassBuilder implements Builder {
-  Set<AssetId> mainInputs = new Set();
 
   Logger _log = new Logger('SassBuilder');
 
+  Set<AssetId> _mainInputs;
+
+  Set<AssetId> get mainInputs {
+    if (_mainInputs == null) {
+      var sassBuildFile = new File(join(Directory.current.path, '.dart_tool', 'sass_build_main_inputs.json'));
+      if (sassBuildFile.existsSync()) {
+        var mainInputsJson = sassBuildFile.readAsStringSync();
+        mainInputsJson = mainInputsJson.isEmpty ? '[]' : mainInputsJson;
+        List mainInputsList = JSON.decode(mainInputsJson);
+        mainInputsList = mainInputsList == null || mainInputsList.isEmpty ? [] : mainInputsList;
+        _mainInputs = new Set();
+        for (var mainInputStr in mainInputsList) {
+          _mainInputs.add(new AssetId.parse(mainInputStr));
+        }
+      } else {
+        _mainInputs = new Set();
+      }
+    }
+    return _mainInputs;
+  }
+
+  _saveMainInputs() {
+    var sassBuildFile = new File(join(Directory.current.path, '.dart_tool', 'sass_build_main_inputs.json'));
+    if (!sassBuildFile.existsSync()) sassBuildFile.createSync(recursive: true);
+
+    _log.info('writing mainInputs: $_mainInputs');
+    sassBuildFile.writeAsStringSync(JSON.encode(_mainInputs.map((mi) => mi.toString()).toList()));
+  }
+
   @override
   Future build(BuildStep buildStep) async {
-    _log.info('processing changed file: ${buildStep.inputId}');
+    var inputId = buildStep.inputId;
+    _log.info('processing changed file: ${inputId}');
+
+    if (!basename(inputId.path).startsWith('_')) {
+      if (!mainInputs.contains(inputId)) {
+        mainInputs.add(inputId);
+        _saveMainInputs();
+      }
+    }
     _log.info('using mainInputs: $mainInputs');
+
     if (mainInputs.isNotEmpty) {
       for (var mainInput in mainInputs) {
+        _log.info('parsing: ${mainInput}');
         var css = await render(mainInput.path, packageResolver: await PackageResolver.current.asSync);
 //        buildStep.writeAsString(_changeExtension(mainInput), css);
-        var file = new File(_changeExtension(mainInput).path);
-        await file.create(recursive: true);
-        await file.writeAsString(css);
+        var file = new File(mainInput.changeExtension('.css').path);
+        file.createSync(recursive: true);
+        file.writeAsString(css);
       }
-      mainInputs = new Set();
     }
   }
 
   @override
-  List<AssetId> declareOutputs(AssetId inputId) {
-    if (!basename(inputId.path).startsWith('_')) mainInputs.add(inputId);
+  Map<String,List<String>> get buildExtensions =>
+    {
 
-    return [_changeExtension(inputId)];
-  }
+    '': const ['.xxxxx']  // since we are writing files without buildStep, we need to have a fake extension
+  };
 }
-
-AssetId _changeExtension(AssetId inputId) => inputId.changeExtension('.css');
