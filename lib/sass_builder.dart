f8b4153018ef43fa2d8bb446eb275fb339a4325a
Luis Vargas <luisvargastijerino@gmail.com>
2017-05-01 21:36:51 -0400

    0.0.3 - upgrade build to `^0.8.0`

--- lib/sass_builder.dart
+++ lib/sass_builder.dart
@@ -1,10 +1,11 @@
 import 'dart:async';
+import 'dart:io';
+
 import 'package:build/build.dart';
 import 'package:logging/logging.dart';
 import 'package:package_resolver/package_resolver.dart';
 import 'package:path/path.dart';
 import 'package:sass/sass.dart';
-import 'dart:io';
 
 class SassBuilder implements Builder {
   Set<AssetId> mainInputs = new Set();
@@ -15,7 +16,7 @@ class SassBuilder implements Builder {
   Future build(BuildStep buildStep) async {
     _log.info('processing changed file: ${buildStep.inputId}');
     _log.info('using mainInputs: $mainInputs');
-    if(mainInputs.isNotEmpty) {
+    if (mainInputs.isNotEmpty) {
       for (var mainInput in mainInputs) {
         var css = await render(mainInput.path, packageResolver: await PackageResolver.current.asSync);
 //        buildStep.writeAsString(_changeExtension(mainInput), css);
@@ -29,7 +30,7 @@ class SassBuilder implements Builder {
 
   @override
   List<AssetId> declareOutputs(AssetId inputId) {
-    if(!basename(inputId.path).startsWith('_')) mainInputs.add(inputId);
+    if (!basename(inputId.path).startsWith('_')) mainInputs.add(inputId);
 
     return [_changeExtension(inputId)];
   }
