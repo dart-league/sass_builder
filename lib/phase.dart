87cf7c6491e06a0e71770dc91cc20eea75f00ce1
Kevin Moore <kevmoo@google.com>
2017-10-06 19:54:05 -0700

    Use more finals

--- lib/phase.dart
+++ lib/phase.dart
@@ -1,7 +1,7 @@
 import 'package:build_runner/build_runner.dart';
 import 'package:sass_builder/sass_builder.dart';
 
-var _graph = new PackageGraph.forThisPackage();
+final _graph = new PackageGraph.forThisPackage();
 
 BuildAction get sassBuildAction =>
     new BuildAction(new SassBuilder(), _graph.root.name,
