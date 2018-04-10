fcca851277dcb52ebb1ab3121c71e220fec782ea
Aaron Lademann <aaron.lademann@gmail.com>
2018-04-10 08:29:09 -0500

    Improve outputStyle experience for users

--- lib/sass_builder.dart
+++ lib/sass_builder.dart
@@ -37,9 +37,9 @@ class SassBuilder implements Builder {
 
   SassBuilder({String outputExtension: '.css', String outputStyle})
       : this._outputExtension = outputExtension,
-        this._outputStyle = outputStyle ?? _defaultOutputStyle;
+        this._outputStyle = outputStyle ?? _defaultOutputStyle.toString();
 
-  static final _defaultOutputStyle = sass.OutputStyle.expanded.toString();
+  static final _defaultOutputStyle = sass.OutputStyle.expanded;
 
   @override
   Future build(BuildStep buildStep) async {
@@ -61,14 +61,29 @@ class SassBuilder implements Builder {
     _log.fine('compiling file: ${tempAssetPath}');
     var cssOutput = sass.compile(tempAssetPath,
         packageResolver: new SyncPackageResolver.root(tempDir.packagesDir.uri),
-        style: this._outputStyle == _defaultOutputStyle
-            ? sass.OutputStyle.expanded
-            : sass.OutputStyle.compressed);
+        style: _getValidOutputStyle());
 
     // Write the builder output
     var outputId = inputId.changeExtension(_outputExtension);
     buildStep.writeAsString(outputId, '${cssOutput}\n');
     _log.fine('wrote css file: ${outputId.path}');
+  }
+
+  /// Returns a valid `OutputStyle` value to the `style` argument of [sass.compile] during a [build].
+  ///
+  /// * If [_outputStyle] is not `sass.OutputStyle.compressed` or `sass.OutputStyle.expanded`,
+  ///   a warning will be logged informing the user that the [_defaultOutputStyle] will be used.
+  sass.OutputStyle _getValidOutputStyle() {
+    if (this._outputStyle == sass.OutputStyle.compressed.toString()) {
+      return sass.OutputStyle.compressed;
+    } else if (this._outputStyle == sass.OutputStyle.expanded.toString()) {
+      return sass.OutputStyle.expanded;
+    } else {
+      _log.warning('Unknown outputStyle provided: "${this._outputStyle}". '
+          'Supported values are: "expanded" and "compressed". '
+          'The default value of "${_defaultOutputStyle.toString()}" will be used.');
+      return _defaultOutputStyle;
+    }
   }
 
   @override
